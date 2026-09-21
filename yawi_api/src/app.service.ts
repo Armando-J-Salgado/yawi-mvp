e07c05a89b09744662c12cdfd304c8781b2632d7
Armando-J-Salgado <20245138@esen.edu.sv>
2026-09-21 15:43:10 -0600

    fix: Gitleaks detections, prettier formatting

--- yawi_api/src/app.service.ts
+++ yawi_api/src/app.service.ts
@@ -3,10 +3,7 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
-
-  constructor(
-    private readonly configService: ConfigService,
-  ) {}
+  constructor(private readonly configService: ConfigService) {}
 
   getHello(): string {
     console.log(this.configService.get('NODE_ENV'));
